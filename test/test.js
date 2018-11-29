a2eacf762effe2aa9a641d257c04fdcb0dbd221d
Auke van Slooten <auke@poefke.nl>
2018-11-29 13:59:37 +0100

    refactored to use the curriculum.js code

--- test/test.js
+++ test/test.js
@@ -1,25 +1,22 @@
-var Ajv = require('ajv');
-var ajv = new Ajv({
-	'extendRefs': true,
-	'allErrors': true,
-	'jsonPointers': true
-});
-var fs = require('fs');
-var validate = null;
-var data = {};
-var ids = {};
-var types = {};
+	var Ajv = require('ajv');
+	var ajv = new Ajv({
+		'extendRefs': true,
+		'allErrors': true,
+		'jsonPointers': true
+	});
+	var validate = null;
 
 	ajv.addKeyword('itemTypeReference', {
 		validate: function(schema, data, parentSchema, dataPath, parentData, propertyName, rootData) {
 			var matches = /.*\#\/definitions\/(.*)/g.exec(schema);
 			if (matches) {
-				var result = types[data] == matches[1];
+				var result = curriculum.types[data] == matches[1];
 /*
 				if (!result) {
 					console.log(schema);
 					console.log(data);
 					console.log(matches);
+					console.log(dataPath);
 				}
 */
 				return result;
@@ -28,52 +25,18 @@ var types = {};
 		}
 	});
 
-	function loadSchema(name, dir='') {
-		var context = fs.readFileSync(name,'utf-8')
-		var schema = JSON.parse(context);
-
-		var properties = Object.keys(schema.properties);
-		properties.forEach(function(propertyName) {
-			if (typeof schema.properties[propertyName]['#file'] != 'undefined') {
-				var file = schema.properties[propertyName]['#file'];
-				var fileData = fs.readFileSync(dir+file, 'utf-8');
-					console.log(propertyName+': reading dir'+file);
-					data[propertyName] = JSON.parse(fileData);
-					if (typeof data[propertyName] == 'undefined') {
-						console.log(propertyName+' not parsed correctly');
-					} else if (typeof data[propertyName].length == 'undefined') {
-						console.log(propertyName+' has no length');
-					} else {
-						console.log(data[propertyName].length + ' items found');
-					}
-					data[propertyName].forEach(function(entity) {
-						if (ids[entity.id]) {
-							console.log('Duplicate id in '+propertyName+': '+entity.id, 
-								ids[entity.id], entity);
-						} else {
-							ids[entity.id] = entity;
-							types[entity.id] = propertyName;
-						}
-					});
-			} else {
-				console.log('skipping '+propertyName);
-			}
-		});
-		return schema;
-	}
-
-	var kernSchema = loadSchema('kern/context.json','kern/');
-	var vakkenSchema = loadSchema('context.json');
+	var curriculum   = require('../lib/curriculum.js');
+	var kernSchema   = curriculum.loadSchema('kern/context.json','kern/');
+	var vakkenSchema = curriculum.loadSchema('context.json');
 
 	var valid = ajv.addSchema(kernSchema, 'http://curriculum.slo.nl/schemas/kern')
 	               .addSchema(vakkenSchema, 'http://curriculum.slo.nl/schemas/vakken')
-	               .validate('http://curriculum.slo.nl/schemas/vakken', data);
+	               .validate('http://curriculum.slo.nl/schemas/vakken', curriculum.data);
 
 	if (!valid) {
 		ajv.errors.forEach(function(error) {
 			console.log(error.dataPath+': '+error.message);
 		});
-//		console.log(ajv.errorsText());
 	} else {
 		console.log('data is valid!');
 	}
